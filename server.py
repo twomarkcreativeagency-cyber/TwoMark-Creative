097d4ba34500c05c33dbf17aebf96037520054d6
twomarkcreativeagency-cyber <twomarkcreativeagency@gmail.com>
2025-11-10 13:20:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,12 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+import os
+
+mongo_url = os.environ.get('MONGO_URL')
+if not mongo_url:
+    raise ValueError("MONGO_URL environment variable is not set.")
+
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
